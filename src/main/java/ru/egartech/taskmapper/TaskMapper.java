db0955cdb4faf10e86ef80e0ff4a9ed16982b0a2
Александр Стенькин <astenkin@egartech.ru>
2022-09-05 15:51:58 +0300

    Замапил getTasks и обновил конфигурацию Мапера

--- src/main/java/ru/egartech/taskmapper/TaskMapper.java
+++ src/main/java/ru/egartech/taskmapper/TaskMapper.java
@@ -1,12 +1,17 @@
 package ru.egartech.taskmapper;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Component;
 import ru.egartech.taskmapper.dto.task.TaskDto;
+import ru.egartech.taskmapper.dto.task.TasksDto;
 
-@Component
+import java.util.List;
+
+
 @RequiredArgsConstructor
 public class TaskMapper {
 
@@ -15,6 +20,11 @@ public class TaskMapper {
     @SneakyThrows
     public TaskDto map(String json) {
         return mapper.readValue(json, TaskDto.class);
+    }
+
+    @SneakyThrows
+    public TasksDto mapList(String json) {
+        return mapper.readValue(json, TasksDto.class);
     }
 
 }
---
+++ src/main/java/ru/egartech/taskmapper/dto/task/TasksDto.java
@@ -0,0 +1,31 @@
+package ru.egartech.taskmapper.dto.task;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.egartech.taskmapper.exception.CustomFieldNotFoundException;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TasksDto {
+
+    @JsonProperty("tasks")
+    private List<TaskDto> tasks;
+
+    @JsonIgnore
+    public TaskDto get() {
+        return tasks.stream()
+                .findFirst()
+                .orElseThrow(
+                        () -> new CustomFieldNotFoundException("tasks")
+                );
+    }
+
+}
