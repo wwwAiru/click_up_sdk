c22e7a8ceffb471d55cf9a0d73304c793be4cc9e
MVinnikov <MVinnikov@egartech.ru>
2022-09-07 14:12:24 +0300

    Удалил ненужные филды. В AbstractField заменил типы с удаленных на TextFieldDto.

--- src/main/java/ru/egartech/taskmapper/dto/task/customfield/field/AbstractField.java
+++ src/main/java/ru/egartech/taskmapper/dto/task/customfield/field/AbstractField.java
@@ -9,29 +9,25 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import ru.egartech.taskmapper.dto.task.customfield.field.attachment.AttachmentFieldDto;
-import ru.egartech.taskmapper.dto.task.customfield.field.date.DateFieldDto;
 import ru.egartech.taskmapper.dto.task.customfield.field.dropdown.DropdownFieldDto;
-import ru.egartech.taskmapper.dto.task.customfield.field.email.EmailFieldDto;
 import ru.egartech.taskmapper.dto.task.customfield.field.label.LabelsFieldDto;
 import ru.egartech.taskmapper.dto.task.customfield.field.number.NumberFieldDto;
-import ru.egartech.taskmapper.dto.task.customfield.field.phone.PhoneFieldDto;
 import ru.egartech.taskmapper.dto.task.customfield.field.relationship.RelationShipFieldDto;
 import ru.egartech.taskmapper.dto.task.customfield.field.text.TextFieldDto;
-import ru.egartech.taskmapper.dto.task.customfield.field.url.UrlFieldDto;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes({
         @JsonSubTypes.Type(value = AttachmentFieldDto.class, name = "attachment"),
-        @JsonSubTypes.Type(value = DateFieldDto.class, name = "date"),
+        @JsonSubTypes.Type(value = TextFieldDto.class, name = "date"),
         @JsonSubTypes.Type(value = DropdownFieldDto.class, name = "drop_down"),
-        @JsonSubTypes.Type(value = EmailFieldDto.class, name = "email"),
+        @JsonSubTypes.Type(value = TextFieldDto.class, name = "email"),
         @JsonSubTypes.Type(value = LabelsFieldDto.class, name = "labels"),
         @JsonSubTypes.Type(value = NumberFieldDto.class, name = "number"),
-        @JsonSubTypes.Type(value = PhoneFieldDto.class, name = "phone"),
+        @JsonSubTypes.Type(value = TextFieldDto.class, name = "phone"),
         @JsonSubTypes.Type(value = RelationShipFieldDto.class, name = "list_relationship"),
         @JsonSubTypes.Type(value = TextFieldDto.class, name = "short_text"),
-        @JsonSubTypes.Type(value = UrlFieldDto.class, name = "url"),
+        @JsonSubTypes.Type(value = TextFieldDto.class, name = "url"),
 })
 @Data
 @SuperBuilder
