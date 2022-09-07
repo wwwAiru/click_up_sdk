137a6ef83e49bd7f32e43e68c2e917eada4d009e
Александр Стенькин <astenkin@egartech.ru>
2022-09-07 10:38:58 +0300

    Добавил CreateFieldDto для создания таски с кастом филдами и удалил излишние поля обновления/создания таски

--- src/main/java/ru/egartech/taskmapper/dto/task/RequestTaskDto.java
+++ src/main/java/ru/egartech/taskmapper/dto/task/RequestTaskDto.java
@@ -18,44 +18,7 @@ public abstract class RequestTaskDto {
 
     @JsonProperty("text_content")
     private String textContent;
-
     private String description;
     private Status status;
-
-    @JsonProperty("orderindex")
-    private int orderIndex;
-
-    @JsonProperty("date_created")
-    private String dateCreated;
-
-    @JsonProperty("date_updated")
-    private String dateUpdated;
-
-    @JsonProperty("date_closed")
-    private String dateClosed = null;
-
-    private Creator creator;
-    private AssignerDto[] assigners = {};
-    private Object[] checklists = {};
-    private Object[] tags = {};
-    private Object parent;
-    private Object priority;
-
-    @JsonProperty("due_date")
-    private String dueDate;
-
-    @JsonProperty("start_date")
-    private String startDate;
-
-    @JsonProperty("time_estimate")
-    private String timeEstimate;
-
-    @JsonProperty("time_spent")
-    private String timeSpent;
-
-    @JsonProperty("custom_fields")
-    private List<CustomField<?>> customFields;
-
-    private String url;
 
 }
