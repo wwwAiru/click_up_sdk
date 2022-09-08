c3d4317568794a2d46218dc5a89cae0dc867898f
MVinnikov <MVinnikov@egartech.ru>
2022-09-08 11:40:27 +0300

    Изменены значение enum'ов

--- src/main/java/ru/egartech/taskmapper/api/UrlProvider.java
+++ src/main/java/ru/egartech/taskmapper/api/UrlProvider.java
@@ -7,9 +7,9 @@ import lombok.RequiredArgsConstructor;
 public enum UrlProvider {
 
     BASE_URL("https://api.clickup.com/api/v2"),
-    SEARCH_TASK_BY_ID_URL("/task/{id}?include_subtasks={include_subtasks}"),
-    CREATE_TASK("https://api.clickup.com/api/v2/list/{list_id}/task"),
-    SEARCH_TASKS_BY_CUSTOM_FIELDS_URL("/list/{list_id}/task?custom_fields={custom_field_req}");
+    SEARCH_TASK_BY_ID_URL(BASE_URL.getUrl() + "/task/{id}?include_subtasks={include_subtasks}"),
+    CREATE_TASK(BASE_URL.getUrl() + "/list/{list_id}/task"),
+    SEARCH_TASKS_BY_CUSTOM_FIELDS_URL(BASE_URL.getUrl() + "/list/{list_id}/task?custom_fields={custom_field_req}");
 
     @Getter
     private final String url;
