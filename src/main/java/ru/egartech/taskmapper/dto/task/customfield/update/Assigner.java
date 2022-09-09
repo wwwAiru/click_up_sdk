e7a863035142cc3b5a3ac9ad49bb39af52212c53
Александр Стенькин <astenkin@egartech.ru>
2022-09-09 12:13:17 +0300

    Добавил Assigner к задаче для привязки задачи к кому-либо

---
+++ src/main/java/ru/egartech/taskmapper/dto/task/customfield/update/Assigner.java
@@ -0,0 +1,29 @@
+package ru.egartech.taskmapper.dto.task.customfield.update;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor(staticName = "of")
+public class Assigner {
+
+    @JsonProperty("add")
+    private List<String> toAdd = new ArrayList<>();
+    @JsonProperty("rem")
+    private List<String> toRemove = new ArrayList<>();
+
+    public Assigner link(String... args) {
+        toAdd = List.of(args);
+        return this;
+    }
+
+    public Assigner unlink(String... args) {
+        toRemove = List.of(args);
+        return this;
+    }
+
+}
