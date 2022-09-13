0b903c6f54c8cd6479c7b59070c85472f552f398
MVinnikov <MVinnikov@egartech.ru>
2022-09-13 09:17:42 +0300

    Поменял @Value на @ConfigurationProperties, чтобы при внедрении ListTaskClient не выбрасывалось исключение, что нет таких элементов в properties.

--- src/main/java/ru/egartech/sdk/api/ListIdsProperties.java
+++ src/main/java/ru/egartech/sdk/api/ListIdsProperties.java
@@ -1,26 +1,21 @@
 package ru.egartech.sdk.api;
 
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-@Getter
+@Data
+@ConfigurationProperties(prefix = "clickup.list")
 public class ListIdsProperties {
 
-    @Value("${clickup.list.developers}")
     private Integer devs;
 
-    @Value("${clickup.list.analytics}")
     private Integer analytics;
 
-    @Value("${clickup.list.testers}")
     private Integer testers;
-
 
     private final List<Integer> ids = new ArrayList<>();
 
