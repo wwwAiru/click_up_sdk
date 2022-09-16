4f4da25e8420e07bf2e0642ea9c684e91570cefb
MVinnikov <MVinnikov@egartech.ru>
2022-09-16 09:05:22 +0300

    Добавлены абстрактные классы для удержания Spring Boot контекста и для конфигурации тестов.

---
+++ src/test/java/ru/egartech/sdk/AbstractSpringBootContext.java
@@ -0,0 +1,7 @@
+package ru.egartech.sdk;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public abstract class AbstractSpringBootContext {
+}
---
+++ src/test/java/ru/egartech/sdk/config/AbstractBaseConfig.java
@@ -0,0 +1,7 @@
+package ru.egartech.sdk.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+public class AbstractBaseConfig {
+}
