e464301242e86b7f88b66720108e32f09b9a130c
MVinnikov <MVinnikov@egartech.ru>
2022-09-16 10:05:22 +0300

    Изменена валидация для property. Теперь если токены не указаны, то вместо исключения TokensNotSpecifiedException будет использоваться аннотация NotEmpty.

--- src/main/java/ru/egartech/sdk/property/AuthorizationProperties.java
+++ src/main/java/ru/egartech/sdk/property/AuthorizationProperties.java
@@ -3,19 +3,20 @@ package ru.egartech.sdk.property;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import ru.egartech.sdk.exception.token.TokensNotSpecifiedException;
+import org.springframework.validation.annotation.Validated;
 
-import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "clickup.api")
+@Validated
 public class AuthorizationProperties {
-
     private int index = 0;
 
+    @NotEmpty
     private List<String> tokens = new ArrayList<>();
 
     public String pullToken() {
@@ -23,12 +24,4 @@ public class AuthorizationProperties {
         index++;
         return pulledToken;
     }
-
-    @PostConstruct
-    public void init() {
-        if (tokens.isEmpty()) {
-            throw new TokensNotSpecifiedException();
-        }
-    }
-
 }
