b5f1db882dd7c5057e6988092a7d198946d23237
MVinnikov <MVinnikov@egartech.ru>
2022-09-16 09:35:24 +0300

    Рефакторинг. Теперь AuthorizationProperties внедряется через метод.

--- src/main/java/ru/egartech/sdk/config/RestTemplateAutoConfiguration.java
+++ src/main/java/ru/egartech/sdk/config/RestTemplateAutoConfiguration.java
@@ -1,31 +1,27 @@
 package ru.egartech.sdk.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import ru.egartech.sdk.property.AuthorizationProperties;
 import ru.egartech.sdk.api.AuthorizationRequestInterceptor;
+import ru.egartech.sdk.property.AuthorizationProperties;
 
 @Configuration
 @ConditionalOnProperty(prefix = "clickup.api", name = "tokens", matchIfMissing = true)
 @EnableConfigurationProperties(value = AuthorizationProperties.class)
-@RequiredArgsConstructor
 public class RestTemplateAutoConfiguration {
 
-    private final AuthorizationProperties properties;
-
     @Bean
-    public RestTemplate restTemplate() {
+    public RestTemplate restTemplate(AuthorizationProperties properties) {
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getInterceptors().add(authorizationRequestInterceptor());
+        restTemplate.getInterceptors().add(authorizationRequestInterceptor(properties));
         return restTemplate;
     }
 
     @Bean
-    public AuthorizationRequestInterceptor authorizationRequestInterceptor() {
+    public AuthorizationRequestInterceptor authorizationRequestInterceptor(AuthorizationProperties properties) {
         return new AuthorizationRequestInterceptor(properties);
     }
 
