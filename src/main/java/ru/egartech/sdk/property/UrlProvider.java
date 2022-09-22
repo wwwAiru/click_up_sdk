fe1d4a3e277b4b6d31fdf39a1fc0e213c9ccb33e
Александр Стенькин <astenkin@egartech.ru>
2022-09-22 10:08:02 +0300

    Пофиксил URL запроса тасок по кастомным филдам

--- src/main/java/ru/egartech/sdk/property/UrlProvider.java
+++ src/main/java/ru/egartech/sdk/property/UrlProvider.java
@@ -11,7 +11,7 @@ public enum UrlProvider {
     UPDATE_TASK(BASE_URL.getUrl() + "/task/{id}"),
     UPDATE_CUSTOM_FIELD(BASE_URL.getUrl() + "/task/{id}/field/{field_id}"),
     GET_ACCESSIBLE_CUSTOM_FIELDS(BASE_URL.getUrl() + "/list/{list_id}/field"),
-    SEARCH_TASKS_BY_CUSTOM_FIELDS_URL(BASE_URL.getUrl() + "/list/{list_id}/task?includeSubtasks={include_subtasks}&custom_fields={custom_field_req}");
+    SEARCH_TASKS_BY_CUSTOM_FIELDS_URL(BASE_URL.getUrl() + "/list/{list_id}/task?subtasks={include_subtasks}&custom_fields={custom_field_req}");
 
     @Getter
     private final String url;
