5d670af3583f38b2ac92c1b0351f96637a7e3197
MVinnikov <MVinnikov@egartech.ru>
2022-09-27 17:25:43 +0300

    Добавлена dto для предоставления сведений об исключении

---
+++ src/main/java/ru/egartech/sdk/exception/dto/ApiErrorDto.java
@@ -0,0 +1,23 @@
+package ru.egartech.sdk.exception.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ApiErrorDto {
+
+    @JsonProperty("class")
+    private String propertyClass;
+
+    private String message;
+    private String milliseconds;
+
+}
