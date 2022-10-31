45c233be13fa453a3eba5691271d269648b01078
Александр Стенькин <astenkin@egartech.ru>
2022-10-31 16:40:19 +0300

    Добавил AttachmentClient для работы с аттачментами ClickUp

--- src/main/java/ru/egartech/sdk/config/AttachmentClientAutoConfiguration.java
+++ src/main/java/ru/egartech/sdk/config/AttachmentClientAutoConfiguration.java
@@ -5,14 +5,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestTemplate;
 import ru.egartech.sdk.api.AttachmentClient;
 import ru.egartech.sdk.api.impl.AttachmentClientImpl;
-import ru.egartech.sdk.dto.attachment.serialization.ByteArrayResourceSerializer;
 
 @Configuration
-@Import(ByteArrayResourceSerializer.class)
 @ConditionalOnClass(AttachmentClient.class)
 @RequiredArgsConstructor
 public class AttachmentClientAutoConfiguration {
